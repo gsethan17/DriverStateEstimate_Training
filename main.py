1c9242b57326b184a1416b2e578e3cf69c932f9a
Geesung Oh <gsethan17@gmail.com>
2021-09-20 15:32:37 +0900

    change setting

--- main.py
+++ main.py
@@ -532,7 +532,7 @@ if __name__ == '__main__' :
     # driver = 'GeesungOh'
 
     # 500, 800, 1000, 1500, 2000
-    odometer = 500
+    odometer = 800
 
     # ['can', 'front_image', 'side_image', 'bio', 'audio']
     data = 'front_image'
